8e47376be4b7d09175b27e52ee3e7e11d7a053fa
Shivansh <shivanshgtb@gmail.com>
2025-08-23 01:11:53 +0530

    feat: refactor song generation logic to improve request handling and title assignment

--- frontend/src/actions/generation.ts
+++ frontend/src/actions/generation.ts
@@ -1,30 +1,59 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { inngest } from "~/inngest/client";
 import { auth } from "~/lib/auth";
 import { db } from "~/server/db";
 
+export interface GenerateRequest {
+  prompt?: string;
+  lyrics?: string;
+  fullDescribedSong?: string;
+  describedLyrics?: string;
+  instrumental?: boolean;
+}
 
-export async function queueSong() {
+export async function generateSong(generateRequest: GenerateRequest) {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
   if (!session) redirect("/auth/sign-in");
 
+  await queueSong(generateRequest, 7.5, session.user.id);
+  await queueSong(generateRequest, 15, session.user.id);
+
+  revalidatePath("/create");
+}
+
+export async function queueSong(
+  generateRequest: GenerateRequest,
+  guidanceScale: number,
+  userId: string,
+) {
+  let title = "Untitled";
+  if (generateRequest.describedLyrics) title = generateRequest.describedLyrics;
+  if (generateRequest.fullDescribedSong)
+    title = generateRequest.fullDescribedSong;
+
+  title = title.charAt(0).toUpperCase() + title.slice(1);
+
   const song = await db.song.create({
     data: {
-      userId: session.user.id,
-      title: "Test song 1",
-      fullDescribedSong: "Hip-hop song",
+      userId: userId,
+      title: title,
+      prompt: generateRequest.prompt,
+      lyrics: generateRequest.lyrics,
+      describedLyrics: generateRequest.describedLyrics,
+      fullDescribedSong: generateRequest.fullDescribedSong,
+      instrumental: generateRequest.instrumental,
+      guidanceScale: guidanceScale,
+      audioDuration: 180,
     },
   });
 
